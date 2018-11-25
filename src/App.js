5daffd7f60791f41538bf2547574b6625e48764c
r-francisco <rfrancisco@laughlin.com>
2018-11-25 12:24:57 -0600

    Adding in AJAX call to API

--- src/App.js
+++ src/App.js
@@ -3,25 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      jokes: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("http://localhost:8080/jokes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            jokes: result.results
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { error, isLoaded, jokes } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header"></header>
+          <ul>
+            {jokes.map(joke => (
+              <li key={joke.id}>
+                {joke.id} {joke.type} {joke.setup} {joke.punchline}
+              </li>
+            ))}
+          </ul>
+        </div>
+        
+      );
+    }
   }
 }
 
