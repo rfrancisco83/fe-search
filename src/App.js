1304475c3ee230abd06778038af6fcac073a6c5b
r-francisco <rfrancisco@laughlin.com>
2018-11-26 09:30:24 -0600

    error class

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
 
 
     if (error) {
-      return <div>Error: {error.message}</div>;
+      return <div class="danger">Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
